170b77cab32d68727ac3e78b7d68d9897a6bde2c
Chaslinux <chaslinux@gmail.com>
2023-07-06 12:26:53 -0400

    Fix line 8 unary operator expected for characters only.

--- pricemonitor.sh
+++ pricemonitor.sh
@@ -5,7 +5,7 @@ MODEL=$(xrandr --verbose | edid-decode | grep "Product Name" | xargs | cut -c 23
 MONCAPS=$(echo $MODEL | tr [:lower:] [:upper:])
 MONITORSIZE="${MONCAPS//[A-Z]}"
 
-if [ $MONITORSIZE -ge 2 ]; then
+if [[ $MONITORSIZE -ge 2 ]]; then
 	MONITORSIZE=$(echo ${MONITORSIZE:1:2})
 fi
 
