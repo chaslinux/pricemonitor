c21fef63a744a00ba25fd31641bd7fbe649fda9d
Chaslinux <chaslinux@gmail.com>
2023-07-06 12:21:27 -0400

    Test this code across various monitor sizes to see how it pans out.

--- pricemonitor.sh
+++ pricemonitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PRICE="SEE STAFF"
 MODEL=$(xrandr --verbose | edid-decode | grep "Product Name" | xargs | cut -c 23-)
 MONCAPS=$(echo $MODEL | tr [:lower:] [:upper:])
 MONITORSIZE="${MONCAPS//[A-Z]}"
@@ -11,3 +12,34 @@ fi
 echo $MODEL
 echo $MONCAPS
 echo $MONITORSIZE
+
+case $MONITORSIZE in
+	24)
+	PRICE=30
+	;;
+	23)
+	PRICE=20
+	;;
+	22)
+	PRICE=20
+	;;
+	21)
+	PRICE=10
+	;;
+	20)
+	PRICE=10
+	;;
+	19)
+	PRICE=5
+	;;
+	17)
+	PRICE=5
+	;;
+	*)
+	PRICE="SEE STAFF"
+	;;
+
+esac
+
+echo $PRICE
+
