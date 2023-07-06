2568567d68711d2f32917fadf6c1c9bb192929c7
Chaslinux <chaslinux@gmail.com>
2023-07-06 12:06:05 -0400

    This should capitalize the MODEL as MONCAPS.

--- pricemonitor.sh
+++ pricemonitor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 MODEL=$(xrandr --verbose | edid-decode | grep "Product Name" | xargs | cut -c 23-)
-MONITORSIZE="${MODEL//[A-Z]}"
+MONCAPS=$(echo $MODEL | tr [:lower:] [:upper:])
+MONITORSIZE="${MONCAPS//[A-Z]}"
 
 if [ $MONITORSIZE -ge 2 ]; then
 	MONITORSIZE=$(echo ${MONITORSIZE:1:2})
 fi
 
 echo $MODEL
+echo $MONCAPS
 echo $MONITORSIZE
